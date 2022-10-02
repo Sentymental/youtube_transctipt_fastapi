9698ce856c56faad84c90a287eb2464bc3f6fa5f
Lukasz Szwajnoch <luk.szwajnoch95@gmail.com>
2022-10-02 20:51:58 +0200

    Added Database configuration, engine and sessionmaker

--- app/db_config.py
+++ app/db_config.py
@@ -1,4 +1,27 @@
+"""
+Module that contain our database configuration
+"""
+
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+load_dotenv("../.env")
+
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+db_sesion = scoped_session(
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine,
+    )
+)
 
 Base = declarative_base()
+Base.query = db_sesion.query_property()
