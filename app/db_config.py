e16655a215439da061b511928641b56ca8314a75
Lukasz Szwajnoch <luk.szwajnoch95@gmail.com>
2022-10-03 11:05:11 +0200

    Providing comments for variables and usage

--- app/db_config.py
+++ app/db_config.py
@@ -9,12 +9,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+# Load our environmental variables:
 load_dotenv("../.env")
 
+# Database URL:
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
+# Create engine to the database:
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
+# Create session, with scoped_session which will,
+# create our registry of session factory.
 db_sesion = scoped_session(
     sessionmaker(
         autocommit=False,
@@ -23,5 +28,6 @@ db_sesion = scoped_session(
     )
 )
 
+# Declarative base:
 Base = declarative_base()
 Base.query = db_sesion.query_property()
